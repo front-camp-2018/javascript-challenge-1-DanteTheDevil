a97e4e844e7b36cf72299677bf8c8a4fa46b3b74
ya_ron <dr.likle@gmail.com>
2018-10-29 20:40:20 +0200

    Tasks are completed.

--- src/doubleNumbers/index.js
+++ src/doubleNumbers/index.js
@@ -1,4 +1,12 @@
 /* eslint-disable-next-line */
 export const doubleNum = num => {
-  // your logic here...
+  const value = [...`${num}`];
+  const value_1 = value.slice(0, value.length / 2).join('');
+  const value_2 = value.slice(value.length / 2).join('');
+
+  if (value.length > 1 && value_1 === value_2) {
+    return num;
+  } else {
+    return num * 2;
+  }
 };
