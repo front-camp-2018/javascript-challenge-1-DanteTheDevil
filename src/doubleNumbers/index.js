1594b011ee8383d4137f4155decf7b5984add089
ya_ron <dr.likle@gmail.com>
2018-10-30 21:31:03 +0200

    Fixed some variables names.

--- src/doubleNumbers/index.js
+++ src/doubleNumbers/index.js
@@ -1,8 +1,8 @@
 /* eslint-disable-next-line */
 export const doubleNum = num => {
   const array = [...`${num}`];
-  const value_1 = array.slice(0, array.length / 2).join('');
-  const value_2 = array.slice(array.length / 2).join('');
+  const firstPart = array.slice(0, array.length / 2).join('');
+  const secondPart = array.slice(array.length / 2).join('');
 
-  return value_1 === value_2 ? num : num * 2;
+  return firstPart === secondPart ? num : num * 2;
 };
--- src/sortByWeight/index.js
+++ src/sortByWeight/index.js
@@ -1,10 +1,10 @@
 /* eslint-disable-next-line */
 export const sortByWeight = arr => {
   arr.sort((a, b) => {
-    const value_1 = [...a].reduce((sum, current) => sum + +(current), 0);
-    const value_2 = [...b].reduce((sum, current) => sum + +(current), 0);
+    const valueOne = [...a].reduce((sum, current) => sum + +(current), 0);
+    const valueTwo = [...b].reduce((sum, current) => sum + +(current), 0);
 
-    return value_1 > value_2;
+    return valueOne > valueTwo;
   });
   return arr;
 };
