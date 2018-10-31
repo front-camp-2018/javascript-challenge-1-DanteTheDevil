7874410090621329f944d610ab11d6669304b793
ya_ron <dr.likle@gmail.com>
2018-10-31 12:39:47 +0200

    Added some optimisation, rename some variables and added variable for first sum value.

--- src/sortByWeight/index.js
+++ src/sortByWeight/index.js
@@ -1,8 +1,11 @@
 /* eslint-disable-next-line */
 export const sortByWeight = arr => {
+  const firstSum = 0;
+  const sumOfNumb = elem => [...elem].reduce((sum, current) => sum + +(current), firstSum);
+
   arr.sort((a, b) => {
-    const valueOne = [...a].reduce((sum, current) => sum + +(current), 0);
-    const valueTwo = [...b].reduce((sum, current) => sum + +(current), 0);
+    const valueOne = sumOfNumb(a);
+    const valueTwo = sumOfNumb(b);
 
     return valueOne - valueTwo;
   });
