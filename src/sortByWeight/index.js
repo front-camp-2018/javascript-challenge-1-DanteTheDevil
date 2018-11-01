acb07a6075dc49f20f0b152af6ac84ede4bc73d2
ya_ron <dr.likle@gmail.com>
2018-11-01 23:23:04 +0200

    Made sortByWeight more readable.

--- src/sortByWeight/index.js
+++ src/sortByWeight/index.js
@@ -1,7 +1,11 @@
 /* eslint-disable-next-line */
 export const sortByWeight = arr => {
   const firstSum = 0;
-  const sumNumb = elem => [...elem].reduce((sum, current) => sum + +(current), firstSum);
+  const sumNumb = elem => {
+    const elemArr = [...elem];
+
+    return elemArr.reduce((sum, current) => sum + +(current), firstSum);
+  };
 
   return arr.sort((a, b) => sumNumb(a) - sumNumb(b));
 };
